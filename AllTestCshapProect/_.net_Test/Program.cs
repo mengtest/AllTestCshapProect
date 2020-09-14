502b1e384b89b08f6447fcb5b7d49b81a364274a
xtcal <zhangxin9585@163.com>
2020-09-14 16:56:11 +0800

    no message

--- AllTestCshapProect/_.net_Test/Program.cs
+++ AllTestCshapProect/_.net_Test/Program.cs
@@ -9,6 +9,14 @@ namespace _.net
     class Program
     {
         static void Main (string[] args) {
+            int[] arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var query = from n in arr
+                        select new
+                        {
+                            ID = n,
+                            Name = n.ToString()
+                        };
+      
 
         }
 
